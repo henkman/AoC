d77bdaac19b0709706dd7a1cb2b2c05b012cd16e
henkman <henkman@github.com>
2017-12-03 23:25:29 +0100

    2017 3

--- 2017/3/main.go
+++ 2017/3/main.go
@@ -24,7 +24,13 @@ func (p *Point) TurnLeft() {
 }
 
 func (p *Point) Abs() int {
-	return int(math.Abs(float64(p.X)) + math.Abs(float64(p.Y)))
+	return int(math.Abs(float64(p.X)) +
+		math.Abs(float64(p.Y)))
+}
+
+func (p *Point) Neighbour(op Point) bool {
+	return int(math.Abs(float64(p.X-op.X))) <= 1 &&
+		int(math.Abs(float64(p.Y-op.Y))) <= 1
 }
 
 func first(x int) int {
@@ -33,13 +39,12 @@ func first(x int) int {
 	turns := 0
 	straight := 1
 	p := Point{0, 0}
-loop:
 	for {
 		for i := 1; i <= straight; i++ {
 			p.Add(d)
 			n++
 			if n == x {
-				break loop
+				return p.Abs()
 			}
 		}
 		d.TurnLeft()
@@ -49,12 +54,41 @@ loop:
 			turns = 0
 		}
 	}
-	return p.Abs()
 }
 
 func second(x int) int {
-	// TODO
-	return 0
+	type ValPoint struct {
+		Point
+		Val int
+	}
+	d := Point{1, 0}
+	turns := 0
+	straight := 1
+	p := Point{0, 0}
+	points := []ValPoint{
+		ValPoint{Point{0, 0}, 1},
+	}
+	for {
+		for i := 1; i <= straight; i++ {
+			p.Add(d)
+			v := 0
+			for _, op := range points {
+				if p.Neighbour(op.Point) {
+					v += op.Val
+				}
+			}
+			if v > x {
+				return v
+			}
+			points = append(points, ValPoint{p, v})
+		}
+		d.TurnLeft()
+		turns++
+		if turns == 2 {
+			straight++
+			turns = 0
+		}
+	}
 }
 
 func main() {
