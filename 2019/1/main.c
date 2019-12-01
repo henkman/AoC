6e434cd0a54feecdd1247d0f300d3918695d6dde
henkman <henkman@github.com>
2019-12-01 22:19:37 +0100

    fix indent 2019/1

--- 2019/1/main.c
+++ 2019/1/main.c
@@ -3,42 +3,43 @@
 #include <stdio.h>
 #include <math.h>
 
-static int required_fuel(int mass)
+static int
+required_fuel(int mass)
 {
-    return (int)(floorf(mass / 3) - 2);
+	return (int)(floorf(mass / 3) - 2);
 }
 
 int main(int argc, char** argv)
 {
-    if (0) {
-        printf("%d\n", required_fuel(12));
-        printf("%d\n", required_fuel(14));
-        printf("%d\n", required_fuel(1969));
-        printf("%d\n", required_fuel(100756));
-    }
+	if (0) {
+		printf("%d\n", required_fuel(12));
+		printf("%d\n", required_fuel(14));
+		printf("%d\n", required_fuel(1969));
+		printf("%d\n", required_fuel(100756));
+	}
 
-    char buf[256];
-    int first = 0, second = 0;
-    for (;;) {
-        char* line = fgets(&buf[0], sizeof(line), stdin);
-        if (line == NULL) {
-            break;
-        }
-        int mass = atoi(line);
+	char buf[256];
+	int first = 0, second = 0;
+	for (;;) {
+		char* line = fgets(&buf[0], sizeof(line), stdin);
+		if (line == NULL) {
+			break;
+		}
+		int mass = atoi(line);
 		int fuel = required_fuel(mass);
-        first += fuel;
+		first += fuel;
 
 		int sumfuel = fuel;
-		for(;;) {
+		for (;;) {
 			int nf = required_fuel(fuel);
-			if(nf <= 0) {
+			if (nf <= 0) {
 				break;
 			}
 			sumfuel += nf;
 			fuel = nf;
 		}
 		second += sumfuel;
-    }
-    printf("first: %d\n", first);
-    printf("second: %d\n", second);
+	}
+	printf("first: %d\n", first);
+	printf("second: %d\n", second);
 }
